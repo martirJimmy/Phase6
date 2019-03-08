b0f328e915fb70d546188c9e30ebc3a26cfa797d
martirJimmy <jimmy_martir@davincischools.org>
2019-03-08 14:36:55 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+	function nextImage(el){
+		if (el.src.match("Images/soulstone.jpg")){
+		el.src = "Images/timestone.jpg";
+		} else if (el.src.match("Images/timestone.jpg")){
+		el.src = "Images/spacestone.jpg";
+		} else if (el.src.match("Images/spacestone.jpg")){
+		el.src = "Images/soulstone.jpg";
+		} else {
+	}
+}
+
+		
+	function nextImage2(el){
+		if (el.src.match("Images/powerstone.jpg")){
+		el.src = "Images/realitystone.jpg";
+		} else if (el.src.match("Images/realitystone.jpg")){
+		el.src = "Images/mindstone.jpg";
+		} else if (el.src.match("Images/mindstone.jpg")){
+		el.src = "Images/powerstone.jpg";
+		} else {
+	}
+}
+
+	function nextImage3(el){
+		if (el.src.match("Images/realitystone.jpg")){
+		el.src = "Images/mindstone.jpg";
+		} else if (el.src.match("Images/mindstone.jpg")){
+		el.src = "Images/powerstone.jpg";
+		} else if (el.src.match("Images/powerstone.jpg")){
+		el.src = "Images/realitystone.jpg";
+		} else {
+	}
+}
+
+	function nextImage4(el){
+		if (el.src.match("Images/timestone.jpg")){
+		el.src = "Images/realitystone.jpg";
+		} else if (el.src.match("Images/realitystone.jpg")){
+		el.src = "Images/powerstone.jpg";
+		} else if (el.src.match("Images/powerstone.jpg")){
+		el.src = "Images/timestone.jpg";
+		} else {
+	}
+}
+
+	function nextImage5(el){
+		if (el.src.match("Images/spacestone.jpg")){
+		el.src = "Images/realitystone.jpg";
+		} else if (el.src.match("Images/realitystone.jpg")){
+		el.src = "Images/powerstone.jpg";
+		} else if (el.src.match("Images/powerstone.jpg")){
+		el.src = "Images/spacestone.jpg";
+		} else {
+	}
+}
+
+	function nextImage6(el){
+		if (el.src.match("Images/mindstone.jpg")){
+		el.src = "Images/soulstone.jpg";
+		} else if (el.src.match("Images/soulstone.jpg")){
+		el.src = "Images/timestone.jpg";
+		} else if (el.src.match("Images/timestone.jpg")){
+		el.src = "Images/mindstone.jpg";
+		} else {
+	}
+}
+
+		
+
+
+	
